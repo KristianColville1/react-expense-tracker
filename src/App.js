0f2cd1bd5851e93451cc15e07b5217d1f0bf1d2d
KristianColville1 <kristiancolville1@gmail.com>
2023-04-20 19:15:31 +0100

    Added bootstrap and set up for ExpenseItem

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
     return (
-        <div>
-            <h2>Let's get started!</h2>
+        <div className="mt-2">
+            <h2 className="w-100 text-center">React Expense Tracker</h2>
+
+            <div className="container text-bg-light rounded">
+            <ExpenseItem></ExpenseItem>
+            </div>
         </div>
     );
 }
