4e19c2515cbb69c328bb50467ff45a4fbc6304d2
KristianColville1 <kristiancolville1@gmail.com>
2023-04-20 19:16:04 +0100

    Initial commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,18 @@
+import "./ExpenseItem.css";
+
+function ExpenseItem() {
+    const expenseDate = new Date(2023, 2, 28);
+    const expenseTitle = "Car Insurance";
+    const expenseAmount = 200;
+    return (
+        <div className="expense-item">
+            <div>{expenseDate.toISOString()}</div>
+            <div class="expense-item__description">
+                <h2>{expenseTitle}</h2>
+                <div className="expense-item__price">€{expenseAmount}</div>
+            </div>
+        </div>
+    );
+}
+
+export default ExpenseItem;
