ddfb6efff08b2b7c43e05496814a90a156f9d2ca
KristianColville1 <kristiancolville1@gmail.com>
2023-04-20 20:46:41 +0100

    Added styling to expenses component for displaying nested ExpenseItems

--- src/App.js
+++ src/App.js
@@ -28,10 +28,10 @@ function App() {
         },
     ];
     return (
-        <div className="mt-2">
-            <h2 className="w-100 text-center">React Expense Tracker</h2>
+        <div className="">
+            <h2 className="">React Expense Tracker</h2>
 
-            <div className="container text-bg-light rounded">
+            <div className="">
                 <Expenses items={expenses} />
             </div>
         </div>
