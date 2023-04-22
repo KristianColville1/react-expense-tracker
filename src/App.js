ba8591b37cdef3d49226404de6802421dd423342
KristianColville1 <kristiancolville1@gmail.com>
2023-04-22 12:16:23 +0100

    Built and styled expense form for expense tracker

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
     const expenses = [
@@ -29,11 +30,8 @@ function App() {
     ];
     return (
         <div className="">
-            <h2 className="">React Expense Tracker</h2>
-
-            <div className="">
-                <Expenses items={expenses} />
-            </div>
+            <NewExpense></NewExpense>
+            <Expenses items={expenses} />
         </div>
     );
 }
