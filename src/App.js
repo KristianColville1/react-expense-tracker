54a8130beff1e390496f7a8be76689b0aa58339c
KristianColville1 <kristiancolville1@gmail.com>
2023-04-24 21:54:56 +0100

    Added conditional content after expenses filter

--- src/App.js
+++ src/App.js
@@ -1,38 +1,42 @@
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
+import { useState } from "react";
 
+
+const DUMMY_EXPENSES = [
+    {
+        id: "1a",
+        title: "Car Insurance",
+        amount: 300,
+        date: new Date(2023, 2, 12),
+    },
+    {
+        id: "2a",
+        title: "Hair Cuts",
+        amount: 150,
+        date: new Date(2022, 11, 9),
+    },
+    {
+        id: "3a",
+        title: "Home Insurance",
+        amount: 300,
+        date: new Date(2021, 7, 30),
+    },
+    {
+        id: "4a",
+        title: "Health Insurance",
+        amount: 900,
+        date: new Date(2023, 5, 29),
+    },
+];
 function App() {
-    const expenses = [
-        {
-            id: "1",
-            title: "Car Insurance",
-            amount: 300,
-            date: new Date(2023, 2, 12),
-        },
-        {
-            id: "2",
-            title: "Hair Cuts",
-            amount: 150,
-            date: new Date(2023, 11, 9),
-        },
-        {
-            id: "3",
-            title: "Home Insurance",
-            amount: 300,
-            date: new Date(2023, 7, 30),
-        },
-        {
-            id: "4",
-            title: "Health Insurance",
-            amount: 900,
-            date: new Date(2023, 5, 29),
-        },
-    ];
+    const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
-    const addExpenseHandler = expense => {
-        console.log('In App.js');
-        console.log(expense);
-    }
+    const addExpenseHandler = (expense) => {
+        setExpenses(prevExpenses => {
+            return [expense, ...prevExpenses];
+        });
+    };
     return (
         <div>
             <NewExpense onAddExpense={addExpenseHandler}></NewExpense>
